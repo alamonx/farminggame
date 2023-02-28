4732f25fa8d243cfbbc8d38fa7bc63aa61128010
alamonx <48643807+alamonx@users.noreply.github.com>
2023-02-28 09:38:30 -0500

    Update farming.js

--- farming.js
+++ farming.js
@@ -106,26 +106,7 @@ function updateTimer() {
         }
 
         growingSeed = 0;
-        plotTimer.style.display = "none";
-        harvestButton.style.display = "block";
-        plotDiv.style.backgroundColor = "green";
-        plotDiv.innerHTML = "Harvest";
-    } else {
-        plotTimer.innerHTML = timeLeft + "s";
-        setTimeout(updateTimer, 1000);
-    }
-}
-
-function harvest(plot) {
-    let amount = harvestAmount[plot] * profit[plot];
-    gold += amount;
-    document.getElementById("gold").innerHTML = gold;
-    let plotDiv = document.getElementById("plot" + plot);
-    plotDiv.innerHTML = "Plot";
-    plotDiv.style.backgroundImage = "url('" + images[plot][imageIndex[plot]] + "')";
-    plotDiv.style.backgroundColor = "grey";
-    imageIndex[plot] = 0;
-}
+        plotTimer.style.display
 
 function buyUpgrade(upgrade) {
     let upgradeCost = 0;
@@ -193,9 +174,360 @@ function buyUpgrade(upgrade) {
                 plotDiv.style.backgroundColor = "green";
             }
             document.getElementById("upgrade3").style.display = "none";
+            for (let i = 1; i <= 8; i++) {
+                let plotTimer = document.getElementById("plotTimer" + i);
+                let harvestButton = document.getElementById("harvestButton" + i);
+                let plotDiv = document.getElementById("plot" + i);
+
+                if (plotTimer.style.display !== "none") {
+                    plotTimer.style.display = "none";
+                    harvestButton.style.display = "block";
+                    plotDiv.style.backgroundColor = "green";
+                    plotDiv.innerHTML = "Harvest";
+                }
+            }
             break;
     }
 
-    updateUpgradeButtons();
+    function updateUpgradeButtons() {
+let upgradeButton1 = document.getElementById("upgrade1");
+let upgradeButton2 = document.getElementById("upgrade2");
+let upgradeButton3 = document.getElementById("upgrade3");
+        if (gold >= 100 && upgradeButton1.style.display === "none") {
+    upgradeButton1.style.display = "block";
 }
+
+if (gold >= 500 && upgradeButton2.style.display === "none") {
+    upgradeButton2.style.display = "block";
+}
+
+if (gold >= 1000 && upgradeButton3.style.display === "none") {
+    upgradeButton3.style.display = "block";
+}
+        }
+
+function showMarketplace() {
+document.getElementById("marketplace").style.display = "block";
+document.getElementById("inventory").style.display = "none";
+}
+
+function showInventory() {
+document.getElementById("marketplace").style.display = "none";
+document.getElementById("inventory").style.display = "block";
+}
+
+function buyItem(item) {
+let itemCost = 0;
+let itemName = "";
+let itemDesc = "";
+let itemButton = "";
+switch (item) {
+case 1:
+itemCost = 10;
+itemName = "Watering Can";
+itemDesc = "This item allows you to water your plants to speed up growth.";
+itemButton = "useItem(1)";
+break;
+case 2:
+itemCost = 50;
+itemName = "Hoe";
+itemDesc = "This item allows you to till unused plots for planting.";
+itemButton = "useItem(2)";
+break;
+}
+if (gold < itemCost) {
+    alert("You don't have enough gold to buy this item!");
+    return;
+}
+
+gold -= itemCost;
+document.getElementById("gold").innerHTML = gold;
+
+let newItem = {name: itemName, description: itemDesc, button: itemButton};
+let inventory = JSON.parse(localStorage.getItem("inventory"));
+
+if (inventory === null) {
+    inventory = [];
+}
+
+inventory.push(newItem);
+localStorage.setItem("inventory", JSON.stringify(inventory));
+
+updateInventory();
+}
+
+function useItem(item) {
+let inventory = JSON.parse(localStorage.getItem("inventory"));
+let itemName = "";
+switch (item) {
+case 1:
+itemName = "Watering Can";
+break;
+case 2:
+itemName = "Hoe";
+break;
+}
+for (let i = 0; i < inventory.length; i++) {
+    if (inventory[i].name === itemName) {
+        inventory.splice(i, 1);
+        localStorage.setItem("inventory", JSON.stringify(inventory));
+        break;
+    }
+}
+
+updateInventory();
+            growingSeed = 0;
+        plotTimer.style.display = "none";
+        harvestButton.style.display = "block";
+        plotDiv.style.backgroundColor = "green";
+        plotDiv.innerHTML = "Harvest";
+    } else {
+        plotTimer.innerHTML = timeLeft + "s";
+    }
+
+    setTimeout(updateTimer, 1000);
+}
+
+function harvest(plot) {
+    let amount = harvestAmount[plot];
+    let plant = plants[plot];
+
+    plants[plot] = 0;
+    let plotDiv = document.getElementById("plot" + plot);
+    plotDiv.style.backgroundColor = "brown";
+    plotDiv.innerHTML = "Plot";
+
+    let profitAmount = amount * plant * profit[plant];
+    gold += profitAmount;
+    document.getElementById("gold").innerHTML = gold;
+
+    let message = "You harvested " + amount + " " + plants[plant] + " for " + profitAmount + " gold.";
+    alert(message);
+}
+
+function updateUpgradeButtons() {
+    let upgradeButton1 = document.getElementById("upgrade1");
+    let upgradeButton2 = document.getElementById("upgrade2");
+    let upgradeButton3 = document.getElementById("upgrade3");
+
+    if (gold >= 100 && upgradeButton1.style.display === "none") {
+        upgradeButton1.style.display = "block";
+    }
+
+    if (gold >= 500 && upgradeButton2.style.display === "none") {
+        upgradeButton2.style.display = "block";
+    }
+
+    if (gold >= 1000 && upgradeButton3.style.display === "none") {
+        upgradeButton3.style.display = "block";
+    }
+}
+
+function showMarketplace() {
+    document.getElementById("marketplace").style.display = "block";
+    document.getElementById("inventory").style.display = "none";
+}
+
+function showInventory() {
+    document.getElementById("marketplace").style.display = "none";
+    document.getElementById("inventory").style.display = "block";
+}
+
+function buyItem(item) {
+    let itemCost = 0;
+    let itemName = "";
+    let itemDesc = "";
+    let itemButton = "";
+
+    switch (item) {
+        case 1:
+            itemCost = 10;
+            itemName = "Watering Can";
+            itemDesc = "This item allows you to water your plants to speed up growth.";
+            itemButton = "useItem(1)";
+            break;
+        case 2:
+            itemCost = 50;
+            itemName = "Hoe";
+            itemDesc = "This item allows you to till unused plots for planting.";
+            itemButton = "useItem(2)";
+            break;
+    }
+
+    if (gold < itemCost) {
+        alert("You don't have enough gold to buy this item!");
+        return;
+    }
+
+    gold -= itemCost;
+    document.getElementById("gold").innerHTML = gold;
+
+    let newItem = { name: itemName, description: itemDesc, button: itemButton };
+    let inventory = JSON.parse(localStorage.getItem("inventory"));
+
+    if (inventory === null) {
+        inventory = [];
+    }
+
+    inventory.push(newItem);
+    localStorage.setItem("inventory", JSON.stringify(inventory));
+
+    updateInventory();
+}
+
+function useItem(item) {
+    let inventory = JSON.parse(localStorage.getItem("inventory"));
+    let itemName = "";
+
+    switch (item) {
+        case 1:
+            itemName = "Watering Can";
+            break;
+        case 2:
+            itemName = "Hoe";
+            break;
+    }
+
+    for (let i = 0; i < inventory.length; i++) {
+        if (inventory[i].name === itemName) {
+            inventory.splice(i, 1);
+            localStorage.setItem("inventory", JSON.stringify(inventory));
+            break;
+        }
+    }
+
+    updateInventory();
+}
+
+            
+           function updateUpgradeButtons() {
+    let upgradeButton1 = document.getElementById("upgrade1");
+    let upgradeButton2 = document.getElementById("upgrade2");
+    let upgradeButton3 = document.getElementById("upgrade3");
+
+    if (gold >= 100 && upgradeButton1.style.display === "none") {
+        upgradeButton1.style.display = "block";
+    }
+
+    if (gold >= 500 && upgradeButton2.style.display === "none") {
+        upgradeButton2.style.display = "block";
+    }
+
+    if (gold >= 1000 && upgradeButton3.style.display === "none") {
+        upgradeButton3.style.display = "block";
+    }
+}
+
+function showMarketplace() {
+    document.getElementById("marketplace").style.display = "block";
+    document.getElementById("inventory").style.display = "none";
+}
+
+function showInventory() {
+    document.getElementById("marketplace").style.display = "none";
+    document.getElementById("inventory").style.display = "block";
+}
+
+function buyItem(item) {
+    let itemCost = 0;
+    let itemName = "";
+    let itemDesc = "";
+    let itemButton = "";
+
+    switch (item) {
+        case 1:
+            itemCost = 10;
+            itemName = "Watering Can";
+            itemDesc = "This item allows you to water your plants to speed up growth.";
+            itemButton = "useItem(1)";
+            break;
+        case 2:
+            itemCost = 50;
+            itemName = "Hoe";
+            itemDesc = "This item allows you to till unused plots for planting.";
+            itemButton = "useItem(2)";
+            break;
+    }
+
+    if (gold < itemCost) {
+        alert("You don't have enough gold to buy this item!");
+        return;
+    }
+
+    gold -= itemCost;
+    document.getElementById("gold").innerHTML = gold;
+
+    let newItem = { name: itemName, description: itemDesc, button: itemButton };
+    let inventory = JSON.parse(localStorage.getItem("inventory"));
+
+    if (inventory === null) {
+        inventory = [];
+    }
+
+    inventory.push(newItem);
+    localStorage.setItem("inventory", JSON.stringify(inventory));
+
+    updateInventory();
+}
+
+function useItem(item) {
+    let inventory = JSON.parse(localStorage.getItem("inventory"));
+    let itemName = "";
+
+    switch (item) {
+        case 1:
+            itemName = "Watering Can";
+            break;
+        case 2:
+            itemName = "Hoe";
+            break;
+    }
+
+    for (let i = 0; i < inventory.length; i++) {
+        if (inventory[i].name === itemName) {
+            inventory.splice(i, 1);
+            localStorage.setItem("inventory", JSON.stringify(inventory));
+            break;
+        }
+    }
+
+    updateInventory();
+}
+
+function updateInventory() {
+    let inventory = JSON.parse(localStorage.getItem("inventory"));
+    let inventoryDiv = document.getElementById("inventoryItems");
+
+    inventoryDiv.innerHTML = "";
+
+    if (inventory === null || inventory.length === 0) {
+        inventoryDiv.innerHTML = "Your inventory is empty.";
+        return;
+    }
+
+    for (let i = 0; i < inventory.length; i++) {
+        let item = document.createElement("div");
+        let itemName = document.createElement("h3");
+        let itemDesc = document.createElement("p");
+        let itemButton = document.createElement("button");
+
+        itemName.innerHTML = inventory[i].name;
+        itemDesc.innerHTML = inventory[i].description;
+        itemButton.setAttribute("onclick", inventory[i].button);
+        itemButton.innerHTML = "Use";
+
+        item.appendChild(itemName);
+        item.appendChild(itemDesc);
+        item.appendChild(itemButton);
+
+        inventoryDiv.appendChild(item);
+    }
+}
+
+updateUpgradeButtons();
+update
+
+           
+
+
 
